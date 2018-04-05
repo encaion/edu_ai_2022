71bdf27a9af19daaa0ffe4fd4169c691384301e0
encaion <encaionkim@gmail.com>
2018-04-05 11:03:20 +0900

    laptop 180405 1103 first commit

---
+++ 00_environment.R
@@ -0,0 +1,9 @@
+#### packages ####
+library("readxl")
+library("dplyr")
+library("data.table")
+
+
+#### options ####
+options(stringsAsFactors = FALSE,
+        scipen = 100)
---
+++ 01_data_import.R
@@ -0,0 +1,3 @@
+text_2014 = read_xlsx("2014_2017.xlsx", sheet = 1, 
+                      col_types = rep("text", 5))
+head(text_2014, 2)
